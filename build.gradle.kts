c39f700fcecb09209577a1221cc379de0cc25a90
limit7412 <git@oxymoron.link>
2021-09-24 17:05:38 +0900

    add build args

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,27 @@ repositories {
 }
 
 dependencies {
-  implementation("org.slf4j:slf4j-simple:1.7.28")
+  implementation("org.slf4j:slf4j-simple:1.7.32")
   implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
+}
+
+allprojects {
+  tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+      freeCompilerArgs = listOf(
+        "-Xinline-classes",
+        "-progressive",
+        "-Xopt-in=kotlin.RequiresOptIn",
+        "-Xopt-in=kotlin.ExperimentalStdlibApi",
+        "-Xopt-in=kotlin.time.ExperimentalTime",
+        "-Xopt-in=kotlinx.coroutines.FlowPreview",
+        "-Xopt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+        "-Xopt-in=kotlin.contracts.ExperimentalContracts",
+        "-Xopt-in=androidx.compose.material.ExperimentalMaterialApi",
+        "-Xopt-in=androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi",
+      )
+    }
+  }
 }
 
 nativeImage {
